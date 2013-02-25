f74ef680b1816cc8cdbf452dba80270a1edb8a8e
halesevic <haris.alesevic@atlantbh.com>
2013-02-25 16:12:39 +0100

    Iteration C4

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#columns #side a', :minimum => 4
+    assert_select '#product_list table tr', :minimum => 4
   end
 
   test "should get new" do
