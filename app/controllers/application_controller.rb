a26ccdb848a6214f42b071048f5e37f65a0dcc32
halesevic <haris.alesevic@atlantbh.com>
2013-03-04 10:23:43 +0100

    Iteration I3 - Limiting Access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
   protect_from_forgery
 
   before_filter :set_time
@@ -15,6 +16,14 @@ class ApplicationController < ActionController::Base
     	cart = Cart.create
     	session[:cart_id] = cart.id
     	cart
+    end
+
+  protected
+
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, :notice => 'Please log in'
+      end
     end
     
 end
